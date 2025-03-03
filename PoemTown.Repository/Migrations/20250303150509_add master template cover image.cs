b3e96af5577bf05cb2595dbeb29c9b251e65f61c
kanatsumi <ducnase171103@fpt.edu.vn>
2025-03-03 22:22:15 +0700

    fix migration

--- PoemTown.Repository/Migrations/20250303150509_add master template cover image.cs
+++ PoemTown.Repository/Migrations/20250303150509_add master template cover image.cs
@@ -11,97 +11,6 @@ namespace PoemTown.Repository.Migrations
         /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
         {
-            migrationBuilder.DropForeignKey(
-                name: "FK_Transactions_PaymentGateways_PaymentGatewayId",
-                table: "Transactions");
-
-            migrationBuilder.DropForeignKey(
-                name: "FK_Transactions_Users_UserId",
-                table: "Transactions");
-
-            migrationBuilder.DropIndex(
-                name: "IX_Transactions_PaymentGatewayId",
-                table: "Transactions");
-
-            migrationBuilder.DropIndex(
-                name: "IX_Transactions_UserId",
-                table: "Transactions");
-
-            migrationBuilder.DropColumn(
-                name: "PaymentGatewayId",
-                table: "Transactions");
-
-            migrationBuilder.DropColumn(
-                name: "UserId",
-                table: "Transactions");
-
-            migrationBuilder.RenameColumn(
-                name: "Status",
-                table: "Transactions",
-                newName: "Token");
-
-            migrationBuilder.RenameColumn(
-                name: "Name",
-                table: "Transactions",
-                newName: "Checksum");
-
-            migrationBuilder.RenameColumn(
-                name: "BalanceBefore",
-                table: "Transactions",
-                newName: "DiscountAmount");
-
-            migrationBuilder.RenameColumn(
-                name: "BalanceAfter",
-                table: "Transactions",
-                newName: "Balance");
-
-            migrationBuilder.AddColumn<string>(
-                name: "AppId",
-                table: "Transactions",
-                type: "nvarchar(max)",
-                nullable: true);
-
-            migrationBuilder.AddColumn<string>(
-                name: "BankCode",
-                table: "Transactions",
-                type: "nvarchar(max)",
-                nullable: true);
-
-            migrationBuilder.AddColumn<DateTimeOffset>(
-                name: "CancelledDate",
-                table: "Orders",
-                type: "datetimeoffset",
-                nullable: true);
-
-            migrationBuilder.AddColumn<string>(
-                name: "OrderCode",
-                table: "Orders",
-                type: "nvarchar(max)",
-                nullable: true);
-
-            migrationBuilder.AddColumn<string>(
-                name: "OrderToken",
-                table: "Orders",
-                type: "nvarchar(max)",
-                nullable: true);
-
-            migrationBuilder.AddColumn<DateTimeOffset>(
-                name: "PaidDate",
-                table: "Orders",
-                type: "datetimeoffset",
-                nullable: true);
-
-            migrationBuilder.AddColumn<Guid>(
-                name: "PaymentGatewayId",
-                table: "Orders",
-                type: "uniqueidentifier",
-                nullable: true);
-
-            migrationBuilder.AddColumn<Guid>(
-                name: "UserEWalletId",
-                table: "OrderDetails",
-                type: "uniqueidentifier",
-                nullable: true);
 
             migrationBuilder.AddColumn<string>(
                 name: "CoverImage",
@@ -150,118 +59,14 @@ namespace PoemTown.Repository.Migrations
                 keyValue: new Guid("a3ee2988-67b2-4017-b63b-a0dae4708359"),
                 columns: new[] { "ConcurrencyStamp", "CreatedTime", "LastUpdatedTime" },
                 values: new object[] { "99fd2912-124c-425a-97a2-03be15c8f2b8", new DateTimeOffset(new DateTime(2025, 3, 3, 22, 5, 8, 819, DateTimeKind.Unspecified).AddTicks(5830), new TimeSpan(0, 7, 0, 0, 0)), new DateTimeOffset(new DateTime(2025, 3, 3, 22, 5, 8, 819, DateTimeKind.Unspecified).AddTicks(5830), new TimeSpan(0, 7, 0, 0, 0)) });
-
-            migrationBuilder.CreateIndex(
-                name: "IX_Orders_PaymentGatewayId",
-                table: "Orders",
-                column: "PaymentGatewayId");
-
-            migrationBuilder.CreateIndex(
-                name: "IX_OrderDetails_UserEWalletId",
-                table: "OrderDetails",
-                column: "UserEWalletId");
-
-            migrationBuilder.AddForeignKey(
-                name: "FK_OrderDetails_UserEWallets_UserEWalletId",
-                table: "OrderDetails",
-                column: "UserEWalletId",
-                principalTable: "UserEWallets",
-                principalColumn: "Id");
-
-            migrationBuilder.AddForeignKey(
-                name: "FK_Orders_PaymentGateways_PaymentGatewayId",
-                table: "Orders",
-                column: "PaymentGatewayId",
-                principalTable: "PaymentGateways",
-                principalColumn: "Id");
         }
 
         /// <inheritdoc />
         protected override void Down(MigrationBuilder migrationBuilder)
         {
-            migrationBuilder.DropForeignKey(
-                name: "FK_OrderDetails_UserEWallets_UserEWalletId",
-                table: "OrderDetails");
-
-            migrationBuilder.DropForeignKey(
-                name: "FK_Orders_PaymentGateways_PaymentGatewayId",
-                table: "Orders");
-
-            migrationBuilder.DropIndex(
-                name: "IX_Orders_PaymentGatewayId",
-                table: "Orders");
-
-            migrationBuilder.DropIndex(
-                name: "IX_OrderDetails_UserEWalletId",
-                table: "OrderDetails");
-
-            migrationBuilder.DropColumn(
-                name: "AppId",
-                table: "Transactions");
-
-            migrationBuilder.DropColumn(
-                name: "BankCode",
-                table: "Transactions");
-
-            migrationBuilder.DropColumn(
-                name: "CancelledDate",
-                table: "Orders");
-
-            migrationBuilder.DropColumn(
-                name: "OrderCode",
-                table: "Orders");
-
-            migrationBuilder.DropColumn(
-                name: "OrderToken",
-                table: "Orders");
-
-            migrationBuilder.DropColumn(
-                name: "PaidDate",
-                table: "Orders");
-
-            migrationBuilder.DropColumn(
-                name: "PaymentGatewayId",
-                table: "Orders");
-
-            migrationBuilder.DropColumn(
-                name: "UserEWalletId",
-                table: "OrderDetails");
-
             migrationBuilder.DropColumn(
                 name: "CoverImage",
                 table: "MasterTemplates");
-
-            migrationBuilder.RenameColumn(
-                name: "Token",
-                table: "Transactions",
-                newName: "Status");
-
-            migrationBuilder.RenameColumn(
-                name: "DiscountAmount",
-                table: "Transactions",
-                newName: "BalanceBefore");
-
-            migrationBuilder.RenameColumn(
-                name: "Checksum",
-                table: "Transactions",
-                newName: "Name");
-
-            migrationBuilder.RenameColumn(
-                name: "Balance",
-                table: "Transactions",
-                newName: "BalanceAfter");
-
-            migrationBuilder.AddColumn<Guid>(
-                name: "PaymentGatewayId",
-                table: "Transactions",
-                type: "uniqueidentifier",
-                nullable: true);
-
-            migrationBuilder.AddColumn<Guid>(
-                name: "UserId",
-                table: "Transactions",
-                type: "uniqueidentifier",
-                nullable: true);
 
             migrationBuilder.UpdateData(
                 table: "Roles",
@@ -304,31 +109,6 @@ namespace PoemTown.Repository.Migrations
                 keyValue: new Guid("a3ee2988-67b2-4017-b63b-a0dae4708359"),
                 columns: new[] { "ConcurrencyStamp", "CreatedTime", "LastUpdatedTime" },
                 values: new object[] { "5bd02788-d5f2-45f9-a46b-48afd95ea096", new DateTimeOffset(new DateTime(2025, 3, 1, 1, 59, 38, 395, DateTimeKind.Unspecified).AddTicks(8146), new TimeSpan(0, 7, 0, 0, 0)), new DateTimeOffset(new DateTime(2025, 3, 1, 1, 59, 38, 395, DateTimeKind.Unspecified).AddTicks(8146), new TimeSpan(0, 7, 0, 0, 0)) });
-
-            migrationBuilder.CreateIndex(
-                name: "IX_Transactions_PaymentGatewayId",
-                table: "Transactions",
-                column: "PaymentGatewayId");
-
-            migrationBuilder.CreateIndex(
-                name: "IX_Transactions_UserId",
-                table: "Transactions",
-                column: "UserId");
-
-            migrationBuilder.AddForeignKey(
-                name: "FK_Transactions_PaymentGateways_PaymentGatewayId",
-                table: "Transactions",
-                column: "PaymentGatewayId",
-                principalTable: "PaymentGateways",
-                principalColumn: "Id");
-
-            migrationBuilder.AddForeignKey(
-                name: "FK_Transactions_Users_UserId",
-                table: "Transactions",
-                column: "UserId",
-                principalTable: "Users",
-                principalColumn: "Id",
-                onDelete: ReferentialAction.Restrict);
         }
     }
 }

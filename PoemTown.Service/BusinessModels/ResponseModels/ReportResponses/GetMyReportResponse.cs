498ca1d1b866483043d24229e195a9650416b989
kanatsumi <ducnase171103@fpt.edu.vn>
2025-03-20 16:41:34 +0700

    resolve the report

--- PoemTown.Service/BusinessModels/ResponseModels/ReportResponses/GetMyReportResponse.cs
+++ PoemTown.Service/BusinessModels/ResponseModels/ReportResponses/GetMyReportResponse.cs
@@ -6,6 +6,7 @@ public class GetMyReportResponse
 {
     public Guid Id { get; set; }
     public string ReportReason { get; set; }
+    public string ResolveResponse { get; set; }
     public ReportStatus Status { get; set; }
     public DateTimeOffset CreatedTime { get; set; }
     public GetPoemInReportResponse Poem { get; set; }

838ad10fb3a54410dfc880f870849dc6f851281d
unknown <kalen153246789@gmail.com>
2025-03-21 15:29:46 +0700

    fix entites for achievement and get my achievement with filter and sort paging

---
+++ PoemTown.Service/BusinessModels/MappingModels/AchievementMapping.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using PoemTown.Repository.Entities;
+using PoemTown.Service.BusinessModels.ResponseModels.AchievementRespponses;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PoemTown.Service.BusinessModels.MappingModels
+{
+    public class AchievementMapping : Profile
+    {
+        public AchievementMapping()
+        {
+            CreateMap<Achievement, GetAchievementResponse>().ReverseMap();
+        }
+    }
+}
---
+++ PoemTown.Service/QueryOptions/FilterOptions/AchievementFilters/GetAchievementFilterOption.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PoemTown.Repository.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PoemTown.Service.QueryOptions.FilterOptions.AchievementFilters
+{
+    public class GetAchievementFilterOption
+    {
+        [FromQuery(Name = "type")]
+        public AchievementType? Type { get; set; }
+        [FromQuery(Name = "rank")]
+        public int? Rank { get; set; }
+    }
+}

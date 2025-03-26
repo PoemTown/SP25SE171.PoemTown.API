04fcf9927c56c8de32a7bf916ae66cdc9cf7e7cb
kanatsumi <ducnase171103@fpt.edu.vn>
2025-03-26 16:07:03 +0700

    return plagiarismFrom when poem is actually get caught plagiarism

--- PoemTown.Service/Services/PoemService.cs
+++ PoemTown.Service/Services/PoemService.cs
@@ -1523,7 +1523,7 @@ public class PoemService : IPoemService
         {
             Score = averageScore,
             IsPlagiarism = IsPoemPlagiarism(averageScore),
-            PlagiarismFrom = plagiarismFromResponses,
+            PlagiarismFrom = IsPoemPlagiarism(averageScore) ? plagiarismFromResponses : null,
         };
     }
 

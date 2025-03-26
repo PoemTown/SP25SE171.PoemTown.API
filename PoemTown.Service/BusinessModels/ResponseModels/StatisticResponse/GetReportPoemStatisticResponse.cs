c1a9cf47248ee4d12c465a7997b32a6403374629
kanatsumi <ducnase171103@fpt.edu.vn>
2025-03-26 14:36:34 +0700

    report users statistic, report poems statisitc, report plagiarism poems statistic

---
+++ PoemTown.Service/BusinessModels/ResponseModels/StatisticResponse/GetReportPoemStatisticResponse.cs
@@ -0,0 +1,15 @@
+﻿using PoemTown.Repository.Enums.Reports;
+
+namespace PoemTown.Service.BusinessModels.ResponseModels.StatisticResponse;
+
+public class GetReportPoemStatisticResponse
+{
+    public int TotalDataSamples { get; set; }
+    public IList<GetReportPoemSampleResponse>? Samples { get; set; }
+}
+
+public class GetReportPoemSampleResponse
+{
+    public ReportStatus Type { get; set; }
+    public int TotalPoems { get; set; }
+}

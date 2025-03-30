c0434aa75c2d9bbf9c951475c430bc1a0b9284f1
kanatsumi <ducnase171103@fpt.edu.vn>
2025-03-31 00:21:47 +0700

    get master template orders statistic and get order status statistic

---
+++ PoemTown.Service/BusinessModels/ResponseModels/StatisticsResponse/GetMasterTemplateOrderStatisticResponse.cs
@@ -0,0 +1,13 @@
+﻿namespace PoemTown.Service.BusinessModels.ResponseModels.StatisticsResponse;
+
+public class GetMasterTemplateOrderStatisticResponse
+{
+    public int TotalDataSamples { get; set; }
+    public IList<GetMasterTemplateOrderSampleResponse>? Samples { get; set; }
+}
+
+public class GetMasterTemplateOrderSampleResponse
+{
+    public string TemplateName { get; set; }
+    public int TotalOrders { get; set; }
+}

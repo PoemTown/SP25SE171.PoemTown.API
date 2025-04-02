9fed1a8e6f0746b4fb67ad428346488bb48d5e77
kanatsumi <ducnase171103@fpt.edu.vn>
2025-04-02 22:29:35 +0700

    create and get announcement, create hub for annoucnement

---
+++ PoemTown.API/Controllers/AnnouncementsController.cs
@@ -0,0 +1,44 @@
+﻿using System.Security.Claims;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using PoemTown.API.Base;
+using PoemTown.Service.BusinessModels.RequestModels.AnnouncementRequests;
+using PoemTown.Service.BusinessModels.ResponseModels.AnnouncementResponses;
+using PoemTown.Service.BusinessModels.ResponseModels.Base;
+using PoemTown.Service.Interfaces;
+
+namespace PoemTown.API.Controllers;
+
+public class AnnouncementsController : BaseController
+{
+    private readonly IAnnouncementService _announcementService;
+    
+    public AnnouncementsController(IAnnouncementService announcementService)
+    {
+        _announcementService = announcementService;
+    }
+    
+    [HttpPost]
+    [Route("v1")]
+    public async Task<ActionResult<BaseResponse>> CreateAnnouncement([FromBody] CreateNewAnnouncementRequest request)
+    {
+        
+        await _announcementService.SendAnnouncementAsync(request);
+        return Created(String.Empty, new BaseResponse(StatusCodes.Status201Created, "Created successfully"));
+    }
+    
+    /// <summary>
+    /// Lấy danh sách thông báo của tôi, yêu cầu đăng nhập
+    /// </summary>
+    /// <returns></returns>
+    [HttpGet]
+    [Route("v1/mine")]
+    [Authorize]
+    public async Task<ActionResult<BaseResponse<IEnumerable<GetAnnouncementResponse>>>> GetUserAnnouncements()
+    {
+        Guid userId = Guid.Parse(User.Claims.First(p => p.Type == "UserId").Value);
+     
+        var announcements = await _announcementService.GetUserAnnouncementsAsync(userId);
+        return Ok(new BaseResponse<IEnumerable<GetAnnouncementResponse>>(StatusCodes.Status200OK, "Get user announcement successfully" ,announcements));
+    }
+}
--- PoemTown.Service/Services/AnnouncementService.cs
+++ PoemTown.Service/Services/AnnouncementService.cs
@@ -1,21 +1,67 @@
-﻿using Microsoft.AspNetCore.SignalR;
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.SignalR;
+using Microsoft.EntityFrameworkCore;
+using PoemTown.Repository.CustomException;
+using PoemTown.Repository.Entities;
+using PoemTown.Repository.Interfaces;
+using PoemTown.Service.BusinessModels.RequestModels.AnnouncementRequests;
+using PoemTown.Service.BusinessModels.ResponseModels.AnnouncementResponses;
 using PoemTown.Service.Interfaces;
 using PoemTown.Service.SignalR;
+using PoemTown.Service.SignalR.IReceiveClients;
 
 namespace PoemTown.Service.Services;
 
 public class AnnouncementService : IAnnouncementService 
 {
-    private readonly IHubContext<AnnouncementHub> _announcementHub;
+    private readonly IHubContext<AnnouncementHub, IAnnouncementClient> _hubContext;
+    private readonly IMapper _mapper;
+    private readonly IUnitOfWork _unitOfWork;
     
-    public AnnouncementService(IHubContext<AnnouncementHub> announcementHub)
+    public AnnouncementService(IHubContext<AnnouncementHub, IAnnouncementClient> hubContext,
+        IUnitOfWork unitOfWork,
+        IMapper mapper)
     {
-        _announcementHub = announcementHub;
+        _hubContext = hubContext;
+        _unitOfWork = unitOfWork;
+        _mapper = mapper;
     }
     
-    /*public async Task SendAnnouncementAsync(string message)
+    public async Task SendAnnouncementAsync(CreateNewAnnouncementRequest request)
     {
-        var connectionId = _announcementHub.
-        await _announcementHub.Clients.All.SendAsync("ReceiveAnnouncement", message);
-    }*/
+        var connectionId = AnnouncementHub.GetConnectionId(request.UserId);
+        
+        // Send SignalR if user is online
+        if (connectionId != string.Empty)
+        {
+            await _hubContext.Clients.Client(connectionId).ReceiveAnnouncement(request);
+        }
+        
+        // Check if user exists
+        User? user = await _unitOfWork.GetRepository<User>().FindAsync(u => u.Id == request.UserId);
+        if (user == null)
+        {
+            throw new CoreException(StatusCodes.Status400BadRequest, "User not found");
+        }
+        
+        await _unitOfWork.GetRepository<Announcement>().InsertAsync(new Announcement
+        {
+            Title = request.Title,
+            Content = request.Content,
+            UserId = request.UserId
+        });
+        
+        await _unitOfWork.SaveChangesAsync();
+    }
+    
+    public async Task<IEnumerable<GetAnnouncementResponse>> GetUserAnnouncementsAsync(Guid userId)
+    {
+        var announcements = await _unitOfWork.GetRepository<Announcement>()
+            .AsQueryable()
+            .Where(a => a.UserId == userId)
+            .ToListAsync();
+
+        return _mapper.Map<IEnumerable<GetAnnouncementResponse>>(announcements);
+    }
 }

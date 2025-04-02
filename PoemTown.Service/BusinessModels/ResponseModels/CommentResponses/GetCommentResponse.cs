999db38c52be9c7f362aa2c0cee21e82704976af
kanatsumi <ducnase171103@fpt.edu.vn>
2025-04-02 16:06:21 +0700

    include user information when comment

--- PoemTown.Service/BusinessModels/ResponseModels/CommentResponses/GetCommentResponse.cs
+++ PoemTown.Service/BusinessModels/ResponseModels/CommentResponses/GetCommentResponse.cs
@@ -1,10 +1,13 @@
-﻿namespace PoemTown.Service.BusinessModels.ResponseModels.CommentResponses;
+﻿using PoemTown.Service.BusinessModels.ResponseModels.UserResponses;
+
+namespace PoemTown.Service.BusinessModels.ResponseModels.CommentResponses;
 
 public class GetCommentResponse
 {
     public Guid Id { get; set; }
     public string Content { get; set; }
     public Guid AuthorCommentId { get; set; }
+    public GetBasicUserInformationResponse Author { get; set; }
     public Guid PoemId { get; set; }
     public Guid? ParentCommentId { get; set; }
     public DateTimeOffset CreatedTime { get; set; }

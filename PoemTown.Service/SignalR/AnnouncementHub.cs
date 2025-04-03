c7a9f25fd199b9e8a95c9fe5ccb42bfcb32b704e
kanatsumi <ducnase171103@fpt.edu.vn>
2025-04-03 14:17:11 +0700

    test new logic announcementHub when establish userconnection

--- PoemTown.Service/SignalR/AnnouncementHub.cs
+++ PoemTown.Service/SignalR/AnnouncementHub.cs
@@ -7,27 +7,49 @@ public class AnnouncementHub : Hub<IAnnouncementClient>
 {
     public static Dictionary<Guid, string> Connections = new();
 
-    public override Task OnConnectedAsync()
+    /*public override Task OnConnectedAsync()
     {
-        string? userIdString = Context.User?.Claims.FirstOrDefault(c => c.Type == "UserId")?.Value;
+        /*string? userIdString = Context.User?.Claims.FirstOrDefault(c => c.Type == "UserId")?.Value;
         if(Guid.TryParse(userIdString, out Guid userId))
         {
             Connections.Add(userId, Context.ConnectionId);
-        }
+        }#1#
+        var connectionId = Context.ConnectionId;
+        Connections.TryAdd(connectionId, connectionId);
         
         return base.OnConnectedAsync();
+    }*/
+    public Task RegisterUserConnection(Guid userId)
+    {
+        Connections[userId] = Context.ConnectionId;
+        return Task.CompletedTask;
     }
-
+    
     public override Task OnDisconnectedAsync(Exception? exception)
     {
-        string? userIdString = Context.User?.Claims.FirstOrDefault(c => c.Type == "UserId")?.Value;
+        /*string? userIdString = Context.User?.Claims.FirstOrDefault(c => c.Type == "UserId")?.Value;
         if(Guid.TryParse(userIdString, out Guid userId))
         {
             Connections.Remove(userId);
-        }
+        }*/
+        var userId = Connections.FirstOrDefault(x => x.Value == Context.ConnectionId).Key;
+        Connections.Remove(userId, out _);
         
         return base.OnDisconnectedAsync(exception);
     }
+    /*public override Task OnDisconnectedAsync(Exception? exception)
+    {
+        /*string? userIdString = Context.User?.Claims.FirstOrDefault(c => c.Type == "UserId")?.Value;
+        if(Guid.TryParse(userIdString, out Guid userId))
+        {
+            Connections.Remove(userId);
+        }#1#
+        var connectionId = Context.ConnectionId;
+        Connections.Remove(connectionId, out _);
+        
+        return base.OnDisconnectedAsync(exception);
+    }*/
+    
     public static string GetConnectionId(Guid? userId)
     {
         if(userId == null)
@@ -37,4 +59,14 @@ public class AnnouncementHub : Hub<IAnnouncementClient>
         
         return Connections.TryGetValue(userId.Value, out var connectionId) ? connectionId : string.Empty;
     }
+    
+    /*public static string GetConnectionId(string connectionId)
+    {
+        if(string.IsNullOrWhiteSpace(connectionId))
+        {
+            return string.Empty;
+        }
+        
+        return Connections.ContainsKey(connectionId) ? connectionId : string.Empty;
+    }*/
 }

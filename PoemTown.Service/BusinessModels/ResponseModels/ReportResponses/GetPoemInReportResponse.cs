c1d9fa989247aeb4b5acb635d768db1371719ed0
kanatsumi <ducnase171103@fpt.edu.vn>
2025-04-04 18:06:49 +0700

    add more return property when get reports

--- PoemTown.Service/BusinessModels/ResponseModels/ReportResponses/GetPoemInReportResponse.cs
+++ PoemTown.Service/BusinessModels/ResponseModels/ReportResponses/GetPoemInReportResponse.cs
@@ -1,9 +1,14 @@
-﻿namespace PoemTown.Service.BusinessModels.ResponseModels.ReportResponses;
+﻿using PoemTown.Repository.Enums.Poems;
+
+namespace PoemTown.Service.BusinessModels.ResponseModels.ReportResponses;
 
 public class GetPoemInReportResponse
 {
     public Guid Id { get; set; }
     public string Title { get; set; }
+    public string Content { get; set; }
+    public PoemType? Type { get; set; } = default!;
+    public string? PoemImage { get; set; } = null;
     public string? Description { get; set; }
     public double? Score { get; set; }
 }

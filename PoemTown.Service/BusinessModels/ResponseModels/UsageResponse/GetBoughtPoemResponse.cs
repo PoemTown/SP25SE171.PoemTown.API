bc0df9912ba614059f017e81af5d51098f1b83ea
qunngh <hoquang1031@gmail.com>
2025-04-08 22:10:46 +0700

    fix get sold and bought poem

--- PoemTown.Service/BusinessModels/ResponseModels/UsageResponse/GetBoughtPoemResponse.cs
+++ PoemTown.Service/BusinessModels/ResponseModels/UsageResponse/GetBoughtPoemResponse.cs
@@ -1,5 +1,6 @@
 ﻿using PoemTown.Repository.Enums.UsageRights;
 using PoemTown.Service.BusinessModels.ResponseModels.PoemResponses;
+using PoemTown.Service.BusinessModels.ResponseModels.SaleVersionResponses;
 using PoemTown.Service.BusinessModels.ResponseModels.UserResponses;
 using System;
 using System.Collections.Generic;
@@ -15,6 +16,7 @@ namespace PoemTown.Service.BusinessModels.ResponseModels.UsageResponse
         public UsageRightStatus Status { get; set; }
         public DateTime? CopyRightValidFrom { get; set; }
         public DateTime? CopyRightValidTo { get; set; }
+        public GetSaleVersionResponse SaleVersion { get; set; } 
         public GetPoemDetailResponse Poem { get; set; }
         public GetBasicUserInformationResponse Buyer { get; set; }
         public GetBasicUserInformationResponse Owner { get; set; }

da0aba56a8c6531634642a2a102c7e12f3abdefb
kanatsumi <ducnase171103@fpt.edu.vn>
2025-04-18 00:31:17 +0700

    fix

--- PoemTown.Service/BusinessModels/ResponseModels/PoemTypeResponses/GetPoemTypeResponse.cs
+++ PoemTown.Service/BusinessModels/ResponseModels/PoemTypeResponses/GetPoemTypeResponse.cs
@@ -3,7 +3,7 @@
 public class GetPoemTypeResponse
 {
     public Guid Id { get; set; }
-    public string? Name { get; set; } = "";
-    public string? Description { get; set; } = "";
-    public string? Color { get; set; } = "";
+    public string? Name { get; set; }
+    public string? Description { get; set; }
+    public string? Color { get; set; }
 }

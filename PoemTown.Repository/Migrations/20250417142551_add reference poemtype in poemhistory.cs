0ffdd7302d4bffc674ea258e3bd307eda218e6ba
kanatsumi <ducnase171103@fpt.edu.vn>
2025-04-17 21:26:53 +0700

    fix reference in poemtype poemhistory

---
+++ PoemTown.Repository/Migrations/20250417142551_add reference poemtype in poemhistory.cs
@@ -0,0 +1,193 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PoemTown.Repository.Migrations
+{
+    /// <inheritdoc />
+    public partial class addreferencepoemtypeinpoemhistory : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_PoemHistories_PoemTypes_TypeId",
+                table: "PoemHistories");
+
+            migrationBuilder.DropIndex(
+                name: "IX_PoemHistories_TypeId",
+                table: "PoemHistories");
+
+            migrationBuilder.DropColumn(
+                name: "TypeId",
+                table: "PoemHistories");
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "PoemTypeId",
+                table: "PoemHistories",
+                type: "uniqueidentifier",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+
+            migrationBuilder.UpdateData(
+                table: "LeaderBoards",
+                keyColumn: "Id",
+                keyValue: new Guid("094de1df-60b1-4a58-878c-dc6909f5262e"),
+                columns: new[] { "CreatedTime", "LastUpdatedTime" },
+                values: new object[] { new DateTimeOffset(new DateTime(2025, 4, 17, 21, 25, 50, 556, DateTimeKind.Unspecified).AddTicks(9184), new TimeSpan(0, 7, 0, 0, 0)), new DateTimeOffset(new DateTime(2025, 4, 17, 21, 25, 50, 556, DateTimeKind.Unspecified).AddTicks(9184), new TimeSpan(0, 7, 0, 0, 0)) });
+
+            migrationBuilder.UpdateData(
+                table: "LeaderBoards",
+                keyColumn: "Id",
+                keyValue: new Guid("094de1df-60b1-4a58-878c-dc6909f5293e"),
+                columns: new[] { "CreatedTime", "LastUpdatedTime" },
+                values: new object[] { new DateTimeOffset(new DateTime(2025, 4, 17, 21, 25, 50, 556, DateTimeKind.Unspecified).AddTicks(9212), new TimeSpan(0, 7, 0, 0, 0)), new DateTimeOffset(new DateTime(2025, 4, 17, 21, 25, 50, 556, DateTimeKind.Unspecified).AddTicks(9212), new TimeSpan(0, 7, 0, 0, 0)) });
+
+            migrationBuilder.UpdateData(
+                table: "Roles",
+                keyColumn: "Id",
+                keyValue: new Guid("89fca251-f021-425b-de62-08dcdfcdb851"),
+                columns: new[] { "CreatedTime", "LastUpdatedTime" },
+                values: new object[] { new DateTimeOffset(new DateTime(2025, 4, 17, 21, 25, 50, 556, DateTimeKind.Unspecified).AddTicks(9077), new TimeSpan(0, 7, 0, 0, 0)), new DateTimeOffset(new DateTime(2025, 4, 17, 21, 25, 50, 556, DateTimeKind.Unspecified).AddTicks(9078), new TimeSpan(0, 7, 0, 0, 0)) });
+
+            migrationBuilder.UpdateData(
+                table: "Roles",
+                keyColumn: "Id",
+                keyValue: new Guid("b74c0a77-a451-4f16-de61-08dcdfcdb851"),
+                columns: new[] { "CreatedTime", "LastUpdatedTime" },
+                values: new object[] { new DateTimeOffset(new DateTime(2025, 4, 17, 21, 25, 50, 556, DateTimeKind.Unspecified).AddTicks(9070), new TimeSpan(0, 7, 0, 0, 0)), new DateTimeOffset(new DateTime(2025, 4, 17, 21, 25, 50, 556, DateTimeKind.Unspecified).AddTicks(9071), new TimeSpan(0, 7, 0, 0, 0)) });
+
+            migrationBuilder.UpdateData(
+                table: "UserRoles",
+                keyColumns: new[] { "RoleId", "UserId" },
+                keyValues: new object[] { new Guid("b74c0a77-a451-4f16-de61-08dcdfcdb851"), new Guid("094de1df-60b1-4a58-878c-dc6909f7350b") },
+                columns: new[] { "CreatedTime", "LastUpdatedTime" },
+                values: new object[] { new DateTimeOffset(new DateTime(2025, 4, 17, 21, 25, 50, 556, DateTimeKind.Unspecified).AddTicks(9120), new TimeSpan(0, 7, 0, 0, 0)), new DateTimeOffset(new DateTime(2025, 4, 17, 21, 25, 50, 556, DateTimeKind.Unspecified).AddTicks(9120), new TimeSpan(0, 7, 0, 0, 0)) });
+
+            migrationBuilder.UpdateData(
+                table: "UserRoles",
+                keyColumns: new[] { "RoleId", "UserId" },
+                keyValues: new object[] { new Guid("89fca251-f021-425b-de62-08dcdfcdb851"), new Guid("a3ee2988-67b2-4017-b63b-a0dae4708359") },
+                columns: new[] { "CreatedTime", "LastUpdatedTime" },
+                values: new object[] { new DateTimeOffset(new DateTime(2025, 4, 17, 21, 25, 50, 556, DateTimeKind.Unspecified).AddTicks(9127), new TimeSpan(0, 7, 0, 0, 0)), new DateTimeOffset(new DateTime(2025, 4, 17, 21, 25, 50, 556, DateTimeKind.Unspecified).AddTicks(9127), new TimeSpan(0, 7, 0, 0, 0)) });
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: new Guid("094de1df-60b1-4a58-878c-dc6909f7350b"),
+                columns: new[] { "ConcurrencyStamp", "CreatedTime", "LastUpdatedTime" },
+                values: new object[] { "4c11de82-da37-4836-bceb-50b848bab7ff", new DateTimeOffset(new DateTime(2025, 4, 17, 21, 25, 50, 556, DateTimeKind.Unspecified).AddTicks(8925), new TimeSpan(0, 7, 0, 0, 0)), new DateTimeOffset(new DateTime(2025, 4, 17, 21, 25, 50, 556, DateTimeKind.Unspecified).AddTicks(8925), new TimeSpan(0, 7, 0, 0, 0)) });
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: new Guid("a3ee2988-67b2-4017-b63b-a0dae4708359"),
+                columns: new[] { "ConcurrencyStamp", "CreatedTime", "LastUpdatedTime" },
+                values: new object[] { "5b6b0f55-30ce-4521-82f7-a7ac9959d2ee", new DateTimeOffset(new DateTime(2025, 4, 17, 21, 25, 50, 556, DateTimeKind.Unspecified).AddTicks(8997), new TimeSpan(0, 7, 0, 0, 0)), new DateTimeOffset(new DateTime(2025, 4, 17, 21, 25, 50, 556, DateTimeKind.Unspecified).AddTicks(8997), new TimeSpan(0, 7, 0, 0, 0)) });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PoemHistories_PoemTypeId",
+                table: "PoemHistories",
+                column: "PoemTypeId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_PoemHistories_PoemTypes_PoemTypeId",
+                table: "PoemHistories",
+                column: "PoemTypeId",
+                principalTable: "PoemTypes",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_PoemHistories_PoemTypes_PoemTypeId",
+                table: "PoemHistories");
+
+            migrationBuilder.DropIndex(
+                name: "IX_PoemHistories_PoemTypeId",
+                table: "PoemHistories");
+
+            migrationBuilder.DropColumn(
+                name: "PoemTypeId",
+                table: "PoemHistories");
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "TypeId",
+                table: "PoemHistories",
+                type: "uniqueidentifier",
+                nullable: true);
+
+            migrationBuilder.UpdateData(
+                table: "LeaderBoards",
+                keyColumn: "Id",
+                keyValue: new Guid("094de1df-60b1-4a58-878c-dc6909f5262e"),
+                columns: new[] { "CreatedTime", "LastUpdatedTime" },
+                values: new object[] { new DateTimeOffset(new DateTime(2025, 4, 17, 21, 18, 2, 260, DateTimeKind.Unspecified).AddTicks(3296), new TimeSpan(0, 7, 0, 0, 0)), new DateTimeOffset(new DateTime(2025, 4, 17, 21, 18, 2, 260, DateTimeKind.Unspecified).AddTicks(3296), new TimeSpan(0, 7, 0, 0, 0)) });
+
+            migrationBuilder.UpdateData(
+                table: "LeaderBoards",
+                keyColumn: "Id",
+                keyValue: new Guid("094de1df-60b1-4a58-878c-dc6909f5293e"),
+                columns: new[] { "CreatedTime", "LastUpdatedTime" },
+                values: new object[] { new DateTimeOffset(new DateTime(2025, 4, 17, 21, 18, 2, 260, DateTimeKind.Unspecified).AddTicks(3317), new TimeSpan(0, 7, 0, 0, 0)), new DateTimeOffset(new DateTime(2025, 4, 17, 21, 18, 2, 260, DateTimeKind.Unspecified).AddTicks(3317), new TimeSpan(0, 7, 0, 0, 0)) });
+
+            migrationBuilder.UpdateData(
+                table: "Roles",
+                keyColumn: "Id",
+                keyValue: new Guid("89fca251-f021-425b-de62-08dcdfcdb851"),
+                columns: new[] { "CreatedTime", "LastUpdatedTime" },
+                values: new object[] { new DateTimeOffset(new DateTime(2025, 4, 17, 21, 18, 2, 260, DateTimeKind.Unspecified).AddTicks(3195), new TimeSpan(0, 7, 0, 0, 0)), new DateTimeOffset(new DateTime(2025, 4, 17, 21, 18, 2, 260, DateTimeKind.Unspecified).AddTicks(3197), new TimeSpan(0, 7, 0, 0, 0)) });
+
+            migrationBuilder.UpdateData(
+                table: "Roles",
+                keyColumn: "Id",
+                keyValue: new Guid("b74c0a77-a451-4f16-de61-08dcdfcdb851"),
+                columns: new[] { "CreatedTime", "LastUpdatedTime" },
+                values: new object[] { new DateTimeOffset(new DateTime(2025, 4, 17, 21, 18, 2, 260, DateTimeKind.Unspecified).AddTicks(3187), new TimeSpan(0, 7, 0, 0, 0)), new DateTimeOffset(new DateTime(2025, 4, 17, 21, 18, 2, 260, DateTimeKind.Unspecified).AddTicks(3188), new TimeSpan(0, 7, 0, 0, 0)) });
+
+            migrationBuilder.UpdateData(
+                table: "UserRoles",
+                keyColumns: new[] { "RoleId", "UserId" },
+                keyValues: new object[] { new Guid("b74c0a77-a451-4f16-de61-08dcdfcdb851"), new Guid("094de1df-60b1-4a58-878c-dc6909f7350b") },
+                columns: new[] { "CreatedTime", "LastUpdatedTime" },
+                values: new object[] { new DateTimeOffset(new DateTime(2025, 4, 17, 21, 18, 2, 260, DateTimeKind.Unspecified).AddTicks(3233), new TimeSpan(0, 7, 0, 0, 0)), new DateTimeOffset(new DateTime(2025, 4, 17, 21, 18, 2, 260, DateTimeKind.Unspecified).AddTicks(3233), new TimeSpan(0, 7, 0, 0, 0)) });
+
+            migrationBuilder.UpdateData(
+                table: "UserRoles",
+                keyColumns: new[] { "RoleId", "UserId" },
+                keyValues: new object[] { new Guid("89fca251-f021-425b-de62-08dcdfcdb851"), new Guid("a3ee2988-67b2-4017-b63b-a0dae4708359") },
+                columns: new[] { "CreatedTime", "LastUpdatedTime" },
+                values: new object[] { new DateTimeOffset(new DateTime(2025, 4, 17, 21, 18, 2, 260, DateTimeKind.Unspecified).AddTicks(3239), new TimeSpan(0, 7, 0, 0, 0)), new DateTimeOffset(new DateTime(2025, 4, 17, 21, 18, 2, 260, DateTimeKind.Unspecified).AddTicks(3239), new TimeSpan(0, 7, 0, 0, 0)) });
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: new Guid("094de1df-60b1-4a58-878c-dc6909f7350b"),
+                columns: new[] { "ConcurrencyStamp", "CreatedTime", "LastUpdatedTime" },
+                values: new object[] { "5de53b5b-a13e-4deb-abe4-4a3ee6b8b184", new DateTimeOffset(new DateTime(2025, 4, 17, 21, 18, 2, 260, DateTimeKind.Unspecified).AddTicks(3053), new TimeSpan(0, 7, 0, 0, 0)), new DateTimeOffset(new DateTime(2025, 4, 17, 21, 18, 2, 260, DateTimeKind.Unspecified).AddTicks(3053), new TimeSpan(0, 7, 0, 0, 0)) });
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: new Guid("a3ee2988-67b2-4017-b63b-a0dae4708359"),
+                columns: new[] { "ConcurrencyStamp", "CreatedTime", "LastUpdatedTime" },
+                values: new object[] { "2450a29a-a2ad-4f0d-92ef-200de81a620b", new DateTimeOffset(new DateTime(2025, 4, 17, 21, 18, 2, 260, DateTimeKind.Unspecified).AddTicks(3114), new TimeSpan(0, 7, 0, 0, 0)), new DateTimeOffset(new DateTime(2025, 4, 17, 21, 18, 2, 260, DateTimeKind.Unspecified).AddTicks(3114), new TimeSpan(0, 7, 0, 0, 0)) });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PoemHistories_TypeId",
+                table: "PoemHistories",
+                column: "TypeId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_PoemHistories_PoemTypes_TypeId",
+                table: "PoemHistories",
+                column: "TypeId",
+                principalTable: "PoemTypes",
+                principalColumn: "Id");
+        }
+    }
+}

dc020462bf56a89560cb4f8930f16861c107ead4
kanatsumi <ducnase171103@fpt.edu.vn>
2025-04-18 00:32:35 +0700

    fix x2

--- PoemTown.Service/BusinessModels/ResponseModels/PoemResponses/GetPoemDetailResponse.cs
+++ PoemTown.Service/BusinessModels/ResponseModels/PoemResponses/GetPoemDetailResponse.cs
@@ -18,7 +18,7 @@ public class GetPoemDetailResponse
     public Guid Id { get; set; }
     public string Title { get; set; }
     public string Content { get; set; }
-    public GetPoemTypeResponse? PoemType { get; set; }
+    public GetPoemTypeResponse? Type { get; set; }
     public string Description { get; set; }
     public int LikeCount { get; set; }
     public int CommentCount { get; set; }

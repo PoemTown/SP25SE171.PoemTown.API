5d67e20315acf37aa31b72fdc0874b2213e42fcf
kanatsumi <ducnase171103@fpt.edu.vn>
2025-04-18 14:42:21 +0700

    Description instead of Message in GetReportMessageResponse

--- PoemTown.Service/BusinessModels/ResponseModels/ReportResponses/GetReportMessageResponse.cs
+++ PoemTown.Service/BusinessModels/ResponseModels/ReportResponses/GetReportMessageResponse.cs
@@ -5,6 +5,6 @@ namespace PoemTown.Service.BusinessModels.ResponseModels.ReportResponses;
 public class GetReportMessageResponse
 {
     public Guid Id { get; set; }
-    public string Message { get; set; } = string.Empty;
+    public string Description { get; set; } = string.Empty;
     public ReportType Type { get; set; }
 }

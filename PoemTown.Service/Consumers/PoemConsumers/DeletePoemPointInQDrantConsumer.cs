b0e1187dc36ea9689a09097217b351e0b6991a81
kanatsumi <ducnase171103@fpt.edu.vn>
2025-04-24 13:50:16 +0700

    delete qdrant point when admin delete poem, delete poetsample poem

---
+++ PoemTown.Service/Consumers/PoemConsumers/DeletePoemPointInQDrantConsumer.cs
@@ -0,0 +1,26 @@
+﻿using MassTransit;
+using PoemTown.Service.Events.PoemEvents;
+using PoemTown.Service.PlagiarismDetector.Interfaces;
+
+namespace PoemTown.Service.Consumers.PoemConsumers;
+
+public class DeletePoemPointInQDrantConsumer : IConsumer<DeletePoemPointInQDrantEvent>
+{
+    private IQDrantService _qDrantService;
+
+    public DeletePoemPointInQDrantConsumer(IQDrantService qDrantService)
+    {
+        _qDrantService = qDrantService;
+    }
+
+    public async Task Consume(ConsumeContext<DeletePoemPointInQDrantEvent> context)
+    {
+        var message = context.Message;
+        if (message.PoemIds == null || message.PoemIds.Count <= 0)
+        {
+            return;
+        }
+
+        await _qDrantService.DeletePoemEmbeddingPoint(message.PoemIds);
+    }
+}

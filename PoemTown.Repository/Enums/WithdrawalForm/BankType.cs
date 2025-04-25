51be8e0d4bc793aa8e2a85bf0bacdc193875042e
kanatsumi <ducnase171103@fpt.edu.vn>
2025-04-26 05:29:05 +0700

    add more type of banktype

--- PoemTown.Repository/Enums/WithdrawalForm/BankType.cs
+++ PoemTown.Repository/Enums/WithdrawalForm/BankType.cs
@@ -4,4 +4,12 @@ public enum BankType
 {
     Momo = 1,
     TpBank = 2,
+    Bidv = 3,
+    Agribank = 4,
+    VietinBank = 5,
+    MbBank = 6,
+    Acb = 7,
+    VpBank = 8,
+    Sacombank = 9,
+    Shb = 10,
 }
